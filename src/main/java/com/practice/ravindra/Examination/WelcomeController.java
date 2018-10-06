30fadbcb5314995bc9184b7464327270fa8a50da
Ravindra Nuggu <ravindra.nuggu@thermofisher.com>
2018-10-06 06:01:25 +0530

    code changes

--- src/main/java/com/practice/ravindra/Examination/WelcomeController.java
+++ src/main/java/com/practice/ravindra/Examination/WelcomeController.java
@@ -1,14 +1,27 @@
 package com.practice.ravindra.Examination;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * Created by ravindra.nuggu on 03/10/18.
  */
 @RestController
+@EnableSwagger2
 public class WelcomeController {
 
+    @ApiOperation(value = "For Saying Hi")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieved list"),
+            @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+    }
+    )
     @GetMapping(value = "/hello")
     public String hello() {
         return "hi";
